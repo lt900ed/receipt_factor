0b8a6d6df837a62fdb4b13738258ba919be994b8
kamuro.iwata <kamuro.iwata@zdh.co.jp>
2023-05-18 19:33:31 +0900

    リセットボタン追加

--- js/receipt_factor.js
+++ js/receipt_factor.js
@@ -12,28 +12,73 @@ fileArea.addEventListener('drop', function(e){
     e.preventDefault();
     fileArea.classList.remove('dragenter');
     var files = e.dataTransfer.files;
-    console.log("DRAG & DROP");
+    console.log('DRAG & DROP');
     console.table(files);
     fileInput.files = files;
     photoPreview('onChange',files);
 });
+function deletePhoto(e) {
+  console.log(e.target);
+  e.target.closest('.containerItem').remove();
+  if (document.getElementsByClassName('containerItem').length == 0) {
+    document.getElementById('btnSubmit').classList.add('imgNotReady');
+    document.getElementById('btnSubmit').classList.add('disable');
+    document.getElementById('btnReset').classList.add('imgNotReady');
+    document.getElementById('btnReset').classList.add('disable');
+  }
+}
+function addPhoto(e) {
+  var preview = document.getElementsByClassName('container')[0];
+  // preview.innerHTML += '<div class="containerItem"><div class="functionWrap"><div class="icon--cross"></div></div><img class="previewImage" src="' + e.target.result + '"></div>';
+  var divContainerItem = document.createElement('div');
+  divContainerItem.setAttribute('class', 'containerItem');
+
+  var divFunctionWrap = document.createElement('div');
+  divFunctionWrap.setAttribute('class', 'functionWrap');
+  divContainerItem.appendChild(divFunctionWrap);
+
+  var divIconCross = document.createElement('div');
+  divIconCross.setAttribute('class', 'icon--cross');
+  divIconCross.addEventListener('click', deletePhoto, false);
+  divFunctionWrap.appendChild(divIconCross);
+
+  var img = document.createElement('img');
+  img.setAttribute('class', 'previewImage');
+  img.setAttribute('src', e.target.result);
+  divContainerItem.appendChild(img);
+  preview.appendChild(divContainerItem);
+};
 function photoPreview(event, fs = null) {
   var files = fs;
   if(files === null){
     files = event.target.files;
   }
-  var preview = document.getElementsByClassName("container")[0];
-  var previewImage = document.getElementsByClassName("previewImage");
-
-  if(previewImage != null) {
-    // preview.removeChild(previewImage);
-  }
+  var cnt_onload = 0;
   for (i = 0; i < files.length; i++) {
     var reader = new FileReader();
-    reader.onload = (function (e) {
-      preview.innerHTML += '<div class="containerItem"><div class="functionWrap"><div class="icon--cross"></div></div><img class="previewImage" src="' + e.target.result + '"></div>';
+    reader.onload = (function(e) {
+      addPhoto(e);
+      ++cnt_onload;
+      if (cnt_onload == files.length){
+        document.getElementById('btnSubmit').classList.remove('imgNotReady');
+        document.getElementById('btnReset').classList.remove('imgNotReady');
+        document.getElementById('btnReset').classList.remove('disable');
+      };
     });
     reader.readAsDataURL(files[i]);
   };
-}
-
+};
+function resetPhoto() {
+  if (document.getElementById('btnReset').classList.contains('imgNotReady')) {
+    console.log('img is not ready.');
+  } else {
+    var preview = document.getElementsByClassName('container')[0];
+    while(preview.firstChild){
+      preview.removeChild(preview.firstChild);
+    }
+    document.getElementById('btnSubmit').classList.add('imgNotReady');
+    document.getElementById('btnSubmit').classList.add('disable');
+    document.getElementById('btnReset').classList.add('imgNotReady');
+    document.getElementById('btnReset').classList.add('disable');
+  }
+};
